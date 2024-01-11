a74816fa6e6026b99b117bdf2a4941dda7f7eb39
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-11 23:56:52 +0100

    push

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- this is a script that retrive name and id of the cities in california
-SELECT id, name FROM cities WHERE states_id = (
+SELECT id, name FROM cities WHERE state_id = (
     SELECT id FROM states WHERE name = 'California'
 )
 ORDER BY id ASC;
