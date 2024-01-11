cb91dc06de0bbbd3629cc1fe882057b62a6550ff
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-11 01:22:48 +0100

    push

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- this part of the script create a table named second_table and gives it 3 arguments
-CREATE DATABASE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
@@ -7,8 +7,7 @@ CREATE DATABASE IF NOT EXISTS second_table (
 
 -- this part of the script insert the rows for the table
 INSERT INTO second_table (id, name, score)
-VALUES  
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+VALUES  (1, 'John', 10),
+        (2, 'Alex', 3),
+        (3, 'Bob', 14),
+        (4, 'George', 8);
